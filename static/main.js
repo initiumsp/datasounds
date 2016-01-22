1b24729ea6ad9bac529a9d5c63be1c137d14d174
Wei Tang <hi@beyond.codes>
2016-01-22 21:47:08 +0800

    Add d3 graph

--- static/main.js
+++ static/main.js
@@ -1,8 +1,14 @@
 d3.csv("data.csv", function(d) {
     return d;
-}, function(error, rows) {
-    var years = rows.map(function(x) { return x.year; });
-    var rows = rows.map(function(x) { return parseFloat(x.temperature); });
+}, function(error, data) {
+    data.forEach(function(d) {
+        d.year = parseInt(d.year);
+        d.temperature = parseFloat(d.temperature);
+        d.date = new Date(d.year, 0, 0, 0, 0, 0, 0);
+    });
+
+    var years = data.map(function(x) { return x.year; });
+    var rows = data.map(function(x) { return x.temperature; });
 
     var min = Math.min.apply(null, rows);
     var max = Math.max.apply(null, rows);
@@ -10,6 +16,60 @@ d3.csv("data.csv", function(d) {
         return Math.floor((x - min) * (30.0 / (max - min)) + 50);
     });
     var msec = timbre.timevalue("bpm120 l8");
+
+    var height = $(window).innerHeight();
+    var width = $(window).innerWidth();
+
+    var fullWidth = width * 5;
+
+    var x = d3.scale.linear().range([0, fullWidth]);
+    var y = d3.scale.linear().range([height, 0]);
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(function(d) { return "" + d; });
+    var yAxis = d3.svg.axis().scale(y).orient("left");
+
+    var area = d3.svg.area()
+        .x(function(d) { return x(d.year); })
+        .y0(height)
+        .y1(function(d) { return y(d.temperature); });
+
+    var line = d3.svg.line()
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(d.temperature); });
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g");
+
+    x.domain(d3.extent(data, function(d) { return d.year; }));
+    y.domain([20, 30]);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0, " + (height - 50) + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("cx", line.x())
+        .attr("cy", line.y())
+        .attr("r", 3.5);
 
     timbre.rec(function(output) {
 
@@ -42,16 +102,28 @@ d3.csv("data.csv", function(d) {
          ).play();
 
         var tickIndex = 0;
+        var startTime = Date.now();
         var lastTime = Date.now();
 
         function animate() {
             var nowTime = Date.now();
             var interval = nowTime - lastTime;
+            var fullInterval = nowTime - startTime;
             if (interval >= msec) {
                 tick(tickIndex);
                 tickIndex += 1;
                 lastTime += msec;
             }
+
+            var movingCount = years.length * (fullWidth - width) / fullWidth;
+            var pageCount = years.length * (width / fullWidth);
+
+            var movePercent = (fullInterval - msec * pageCount / 2) / (msec * movingCount);
+            if (movePercent >= 1) movePercent = 1;
+            if (movePercent <= 0) movePercent = 0;
+
+            svg.attr("transform", "translate(-" + (fullWidth - width) * movePercent + ",0)");
+
             window.requestAnimationFrame(animate);
         }
 
